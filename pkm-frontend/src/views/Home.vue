321797182c7db03f3c7dee8bca5412bc965466a7
Jack Park <jackpark@gmail.com>
2019-07-02 16:57:40 -0700

    cleaned up landing page

--- pkm-frontend/src/views/Home.vue
+++ pkm-frontend/src/views/Home.vue
@@ -2,6 +2,10 @@
     <div>
       <div class="text-center">
         <h1>PKM-1 Prototype</h1>
+        <h2><a href="https://github.com/KnowledgeGarden/tq-pkm-1">tq-pkm-1</a></h2>
+        <hr>
+        <h4>This landing page is up for grabs.</h4>
+        <h4>If the website is marked Private, the prime landing page becomes the Login view</h4>
       </div>
     </div>
 </template>
